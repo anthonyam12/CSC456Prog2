3332121490330589922bd0a23887a1168766b03a
anthonyam12 <anthony.morast@mines.sdsmt.edu>
2015-04-08 21:39:20 -0600

    Made ProcessesScheduler a class and added sort functionality.

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -14,6 +14,7 @@ int main (int argc, char* argv[])
         return 0;
     }    
 
+    ProcessScheduler scheduler;
     
     return 0;
 }
--- simulation/process_scheduler.h
+++ simulation/process_scheduler.h
@@ -9,11 +9,21 @@
 ******************************************************************************/
 
 /******************************************************************************
-                                Includes 
+                        Libraries, Classes, etc. 
 ******************************************************************************/
 #include <iostream>
-
 using namespace std;
+
+class ProcessScheduler 
+{
+    public:
+        void round_robin ( int **processes, int quantum, int num_procs );
+        void priority ( int **processes, int num_procs );
+        void shortest_job_first ( int **processes, int num_procs );
+    private:
+        int **burst_sort ( int **processes, int num_procs );
+        int **priority_sort ( int **processes, int num_procs );
+};
 
 /******************************************************************************
                         Process Scheduling Algorithms 
@@ -25,10 +35,9 @@ using namespace std;
 * This function takes an array of processes and a time quantum indicating how
 * long each process is alotted the CPU. .... 
 ******************************************************************************/
-int **round_robin ( int** processes, int quantum )
+void ProcessScheduler::round_robin ( int** processes, int quantum, int num_procs )
 {
 
-    return processes;
 }
 
 /******************************************************************************
@@ -37,10 +46,9 @@ int **round_robin ( int** processes, int quantum )
 * priority takes an array of processes and their corresponding priorities. The
 * method will implement the Priority scheduling algorithm. ....
 ******************************************************************************/
-int **priority ( int **processes )
+void ProcessScheduler::priority ( int **processes, int num_procs )
 {
 
-    return processes;
 }
 
 /******************************************************************************
@@ -49,8 +57,27 @@ int **priority ( int **processes )
 * shortest_job_first will take an array of processes to be scheduled. The
 * array should contain the process length in an index. ....
 ******************************************************************************/
-int **shortest_job_first ( int **processes )
+void ProcessScheduler::shortest_job_first ( int **processes, int num_procs )
+{
+}
+
+/******************************************************************************
+* Author: Anthony Morast
+*
+* burst_sort sorts the processes in order from shortest to longest for the Shortest
+* Job First scheduling algorithm. 
+******************************************************************************/
+int** ProcessScheduler::burst_sort ( int **processes, int num_procs )
 {
 
-    return processes;
 }
+
+/*****************************************************************************
+* Author: Anthony Morast
+*
+* priority_sort sorts the list of processes based on their priority. 
+******************************************************************************/
+int** ProcessScheduler::priority_sort ( int **processes, int num_procs )
+{
+
+} 
