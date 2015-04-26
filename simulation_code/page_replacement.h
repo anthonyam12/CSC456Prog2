59cd7e04cc3f7bf7b0c741b7fdf2a7dfa101841d
Anthony Morast <anthonymorast33@hotmail.com>
2015-04-25 22:16:08 -0600

    Added the second chance page replacement algorithm

--- simulation_code/page_replacement.h
+++ simulation_code/page_replacement.h
@@ -20,6 +20,16 @@ using namespace std;
 void PrintResults(int **stack, int *reference_string, int num_accesses, int num_frames);
 
 /******************************************************************************
+                            Classes, Structs, etc.
+******************************************************************************/
+class SecondChanceInfo
+{
+    public:
+        int page_contained;
+        int reference_bit;
+};
+
+/******************************************************************************
                         Page Replacement Algorithms
 ******************************************************************************/
 
@@ -164,7 +174,114 @@ void lfu (int num_accesses, int *reference_string, int num_pages, int num_frames
 ******************************************************************************/
 void second_chance (int num_accesses, int *reference_string, int num_pages, int num_frames)
 {
-    
+    // used for output
+    int **each_frame = new int*[num_accesses];
+    for (int i = 0; i < num_accesses; i++)
+        each_frame[i] = new int[num_frames];
+    // tracks the page order at each clock tick
+    SecondChanceInfo *stack = new SecondChanceInfo[num_frames];
+
+    // initialize stack 
+    for (int j = 0; j < num_frames; j++)
+    {
+        stack[j].page_contained = -10;
+        stack[j].reference_bit = 0;
+    }
+
+    // some variables for display
+    int page_faults = 0;
+    int page_hits = 0;
+
+    // will determine if the page is inserted in an empty frame or is in a frame
+    bool added = false;
+    bool contains = true;
+
+    // Execute the page replacement algorithm
+    cout << "--- Second Chance Page Replacement ---" << endl;
+    for (int i = 0; i < num_accesses; i++)
+    {
+        added = false;
+        contains = false;
+        
+        for (int j = 0; j < num_frames; j++)
+        {
+            if (stack[j].page_contained == reference_string[i])
+            {
+                stack[j].reference_bit = 1;
+                contains = true;
+                page_hits++;
+                break;
+            }
+        }
+        if(!contains)
+        {
+            for (int j = 0; j < num_frames; j++)
+            {
+                if (stack[j].page_contained == -10)
+                {
+                    stack[j].page_contained = reference_string[i];
+                    stack[j].reference_bit = 0;
+                    added = true;
+                    page_faults++;
+                    break;
+                }
+            }
+
+            if (!added)
+            {
+                for (int j=0; ;j++)
+                {
+                    j %= num_frames;
+                    // if the reference bit is 0, replace the page
+                    if (stack[j].reference_bit == 0)
+                    {
+                        stack[j].page_contained = reference_string[i];
+
+                        SecondChanceInfo temp;
+                        temp.reference_bit = stack[j].reference_bit;
+                        temp.page_contained = stack[j].page_contained;
+
+                        // move page to end since it is most recently used
+                        for (int k = j; k < num_frames; k++)
+                        {
+                            stack[k].page_contained = stack[k+1].page_contained;
+                            stack[k].reference_bit = stack[k+1].reference_bit;
+                        }
+                        stack[num_frames-1].reference_bit = temp.reference_bit;
+                        stack[num_frames-1].page_contained = temp.page_contained;   
+
+                        break;
+                    }
+                    else // otherwise, clear reference bit, and move it to end
+                    {
+                        SecondChanceInfo temp_;
+                        temp_.reference_bit = 0;
+                        temp_.page_contained = stack[j].page_contained;
+
+                        // move page to end since it is most recently used
+                        for (int k = j; k < num_frames; k++)
+                        {
+                            stack[k].page_contained = stack[k+1].page_contained;
+                            stack[k].reference_bit = stack[k+1].reference_bit;
+                        }
+                        stack[num_frames-1].reference_bit = temp_.reference_bit;
+                        stack[num_frames-1].page_contained = temp_.page_contained; 
+                    }
+                }
+                page_faults++;
+            }
+        }
+
+        for(int j = 0; j < num_frames; j++)
+            each_frame[i][j] = stack[j].page_contained;
+    }
+
+    // print results
+    PrintResults(each_frame, reference_string, num_accesses, num_frames);
+    cout << "Page Faults: " << page_faults << endl;
+    cout << "Page Hits: " << page_hits << endl << endl;
+
+    return;
 }
 
 /******************************************************************************
